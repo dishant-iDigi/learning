17e80d2d84256f56b20caee00114f2a29c512233
Dishant <dishant.phandyal.idigitalize@gmail.com>
2026-09-25 18:35:42 +0530

    new final website

--- sep 25/Simon Game Challenge Starting Files/game.js
+++ sep 25/Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
     var random_number = Math.floor(Math.random() * 4);
@@ -7,10 +8,20 @@ function nextSequence() {
 
     var randomChosenColour = buttonColours[random_number];
     gamePattern.push(randomChosenColour);
-//   ----- flash code below ---
+    //   ----- flash code below ---
     $("#" + randomChosenColour).css("background-color", "white");
     setTimeout(function () {
         $("#" + randomChosenColour).css("background-color", "");
     }, 2);
 }
 
+nextSequence();
+$(".btn").on("click", handler);
+
+function handler(event) {
+    console.log("button clicked id stored !");
+    var userChosenColour = event.target.id;
+
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+}
